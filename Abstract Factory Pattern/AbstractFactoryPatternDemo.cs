cf4c49f16ed63a8e0230ab42b64b29524cf23b70
ABCSHICHI <1370321932@qq.com>
2021-11-22 16:39:02 +0800

    FactoryProducer.cs 工厂生成器，根据字段获取工厂

---
+++ Abstract Factory Pattern/AbstractFactoryPatternDemo.cs
@@ -0,0 +1,59 @@
+﻿using Abstract_Factory_Pattern.AbstractClass;
+using Abstract_Factory_Pattern.Interface;
+using Abstract_Factory_Pattern.Producer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Factory_Pattern
+{
+    class AbstractFactoryPatternDemo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("验证形状工厂的功能");
+            //获取形状工厂
+            AbstractFactory shapeFactory = FactoryProducer.GetFactory("SHAPE");
+            //获取形状为Circle 的对象
+            Shape circle = shapeFactory.GetShape("CIRCLE");
+            //调用Circle的方法
+            circle.draw();
+
+            //获取形状为 Rectangle 的对象
+            Shape rectangle = shapeFactory.GetShape("RECTANGLE");
+            //调用 Rectangle 的方法
+            rectangle.draw();
+
+            //获取形状为 Square 的对象
+            Shape square = shapeFactory.GetShape("SQUARE");
+            //调用 Square 的方法
+            square.draw();
+
+
+            Console.WriteLine();
+            Console.WriteLine("验证颜色工厂的功能");
+
+
+            //获取形状工厂
+            AbstractFactory colorFactory = FactoryProducer.GetFactory("COLOR");
+            //获取形状为Circle 的对象
+            Color red = colorFactory.GetColor("RED");
+            //调用Circle的方法
+            red.fill();
+
+            //获取形状为 Rectangle 的对象
+            Color blue = colorFactory.GetColor("BLUE");
+            //调用 Rectangle 的方法
+            red.fill();
+
+            //获取形状为 Square 的对象
+            Color green = colorFactory.GetColor("GREEN");
+            //调用 Square 的方法
+            red.fill();
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Abstract Factory Pattern/Producer/FactoryProducer.cs
@@ -0,0 +1,24 @@
+﻿using Abstract_Factory_Pattern.AbstractClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Factory_Pattern.Producer
+{
+    /// <summary>
+    /// 工厂生成器，通过传递形状或者颜色信息来获取工厂
+    /// </summary>
+    public class FactoryProducer
+    {
+        public static AbstractFactory GetFactory(string choice)
+        {
+            if (choice.Equals("SHAPE"))
+                return new ShapeFactory();
+            else if (choice.Equals("COLOR"))
+                return new ColorFactory();
+            return null;
+        }
+    }
+}
