eadabb2da530e0b3d32c03fb9de208e80ebc9586
ABCSHICHI <1370321932@qq.com>
2021-11-25 10:51:05 +0800

    添加备注

--- Prototype Pattern/PrototypePatternDemo.cs
+++ Prototype Pattern/PrototypePatternDemo.cs
@@ -11,11 +11,15 @@ namespace Prototype_Pattern
     {
         static void Main(string[] args)
         {
+            //创建实体类对象
             ShapeCache.LoadCache();
+
+            //克隆对象
             Shape cloneShape1 = (Shape)ShapeCache.GetShape("1");
             Shape cloneShape2 = (Shape)ShapeCache.GetShape("2");
             Shape cloneShape3 = (Shape)ShapeCache.GetShape("3");
 
+            //打印克隆对象的属性
             Console.WriteLine("Shape: {0}, {1}, {2}", cloneShape1.GetType(), cloneShape2.GetType(), cloneShape3.GetType());
         }
     }
