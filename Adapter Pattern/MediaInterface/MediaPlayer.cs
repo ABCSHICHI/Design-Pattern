b410376a96fc7cb20ada8499eca7951ae4a0a527
ABCSHICHI <1370321932@qq.com>
2021-11-25 10:59:52 +0800

    适配器模式：兼容不同的接口 为媒体播放器和更高级的媒体播放器创建接口。

---
+++ Adapter Pattern/MediaInterface/MediaPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_Pattern.MediaInterface
+{
+    public interface MediaPlayer
+    {
+        void Play(string audioType, string fileName);
+    }
+}
